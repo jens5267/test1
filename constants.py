e3e6266f1cab7cf8a1671a62a1001bf430a061a6
Jens Reichwein <Jens@dotpower.nl>
2021-02-03 17:55:28 +0100

    test123

---
+++ constants.py
@@ -0,0 +1,30 @@
+WIND_SPEEDS = list(range(26))
+
+R_ROT = 21.6
+TSR = 7.5889
+c_P = 0.42844
+c_T = c_P/TSR
+RHO_AIR = 1.225
+
+VOL_DISP_N = {
+    "m3_rad":1.26*10**-3,
+    "l_rev": 7.93
+}
+MU_MECH_N = 1
+MU_VOL_N = 1
+c_DAMP_N = 1
+RHO_HYD = 1025
+
+VOL_DISP_P = {
+    "m3_rad":8.4*10**-3,
+    "l_rev": 52.8
+}
+MU_MECH_P : 1
+MU_VOL_P = 1
+
+VOL_DISP_M = {
+    "m3_rad":1.42*10**-4,
+    "l_rev": 0.89
+}
+MU_MECH_M : 1
+MU_VOL_M = 1
